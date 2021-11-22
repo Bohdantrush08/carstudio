3bb9c91f9561cd0e247b0e4cdadcdefa6055724b
btrush <btrush@expediagroup.com>
2021-11-22 23:59:37 +0200

    Initial commit

---
+++ src/main/java/com/nazar/carstudio/CarStudioApplication.java
@@ -0,0 +1,13 @@
+package com.nazar.carstudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarStudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarStudioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nazar/carstudio/CarstudioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nazar.carstudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarstudioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
